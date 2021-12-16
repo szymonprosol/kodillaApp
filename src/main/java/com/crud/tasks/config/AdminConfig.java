63a2a056ae7fbb155295aea42a167b1e0588612f
Szymon Rosół <szymon.patryk.rosol@gmail.com>
2021-12-16 19:11:44 +0100

    AdminConfig class has been changed

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -2,8 +2,10 @@ package com.crud.tasks.config;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("heroku")
 @Component
 @Getter
 public class AdminConfig {
